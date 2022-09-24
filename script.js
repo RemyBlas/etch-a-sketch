314c7736e9f1610b7f2fce89389219c3083be63e
Leandro Blasco <lsblasco@hotmail.com>
2022-09-24 20:30:00 -0300

    add eraser mode and reset function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//PENDIENTE:
+//- evento para activar y desactivar el evento de mouseOver así al hacer click sería como apoyar o levantar el 'lápiz'
+
+//Init:
 makeGrid(16);
 
-const grid8 = document.querySelector("#grid8"),
+//Pencil/eraser switch
+let pencilMode = true
+const pencilButton = document.querySelector('#pencil');
+const eraserButton = document.querySelector('#eraser');
+pencilButton.addEventListener('click', () => {pencilMode = true});
+eraserButton.addEventListener('click', () => {pencilMode = false});
+
+//Grid selection options
+const grid8 = document.querySelector('#grid8'),
     grid16 = document.querySelector('#grid16'),
     grid24 = document.querySelector('#grid24'),
     grid32 = document.querySelector('#grid32'),
@@ -12,6 +24,7 @@ grid24.addEventListener('click', function() {makeGrid(24)});
 grid32.addEventListener('click', function() {makeGrid(32)});
 reset.addEventListener('click', resetGrid);
 
+//Grids creation function
 function makeGrid(p) {
     const grid = document.querySelector('.grid');
     while (grid.firstChild) {
@@ -26,20 +39,37 @@ function makeGrid(p) {
         pix.style.opacity = 0.2;
         grid.appendChild(pix);
     }
+    drawing();
+}
 
+//Drawing
+function drawing() {
     const pixels = document.querySelectorAll('.pix');
     pixels.forEach(pix => {
         pix.addEventListener('mouseover', () => {
-            opacityIncrement(pix);
+            if (pencilMode) {
+                opacityIncrement(pix);
+            } else {
+                eraserMode(pix);
+            }
         })
     })
 }
 
 function opacityIncrement(pix) {
-    pix.style.background = "#000000";
+    pix.style.background = '#000000';
     pix.style.opacity = parseFloat(pix.style.opacity) + 0.2;
 }
 
+//Erasing
+function eraserMode(pix) {
+    pix.style.background = '#ffffff';
+    pix.style.opacity = '0.2'
+}
+
+//Reset grid
 function resetGrid() {
-    //scode to reset grid
+    const totalPix = document.getElementsByClassName('pix').length;
+    const currentGrid = Math.sqrt(totalPix);
+    makeGrid(currentGrid);
 }
